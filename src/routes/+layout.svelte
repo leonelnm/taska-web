06d8d515cce476e927a2ec07e383d96ed681b633
leonelnm <elerickmontenegro@gmail.com>
2025-09-18 15:27:10 +0200

    Add favicon SVG and update layout to use it as the icon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,10 @@
 <script lang="ts">
 	import '../app.css';
-	import favicon from '$lib/assets/taska.webp';
-
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="icon" href="favicon.svg" sizes="any" type="image/svg+xml" />
 </svelte:head>
 
 <div class="flex min-h-screen flex-col bg-gray-50 text-gray-900">
